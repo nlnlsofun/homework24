93c37f4018c0a017cad1e8fb3eb5382869d00492
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 21:59:23 +0800

    加入專案檔案。

---
+++ homework24/homework24.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double r; //利率
+    int n, p; //期數、每期投入金額
+    cin >> r >> n >> p;
+
+    double total = p * (pow(1 + r, n) - 1) / r * (1 + r); 
+
+    cout << (int)round(total) << endl; 
+
+    return 0;
+}
